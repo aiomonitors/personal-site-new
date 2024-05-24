cda6de1cd8a7b4450d173bbb24a68a2f95d04304
aiomonitors <sachow168@gmail.com>
2024-05-24 14:30:12 -0400

    feat!: update eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-background">
-        <span className="text-primary">Shihab Chowdhury</span>
-      </div>
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<div className='bg-background'>
+				<span className='text-primary'>Shihab Chowdhury
+				</span>
+			</div>
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,39 @@
-import type { Config } from "tailwindcss";
+/* eslint-disable @typescript-eslint/naming-convention */
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        background: {
-          DEFAULT: 'hsl(var(--background))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          alt: 'hsl(var(--card))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))'
-        },
-        overlay: {
-          DEFAULT: 'hsl(var(--overlay))'
-        }
-      }
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				background: {
+					DEFAULT: 'hsl(var(--background))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					alt: 'hsl(var(--card))',
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+				},
+				overlay: {
+					DEFAULT: 'hsl(var(--overlay))',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
